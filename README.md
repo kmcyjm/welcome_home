e7855b52d002d846f3bc1025d4c42f0a74b3dfbd
yijiaming <yijia@amazon.com>
2017-12-18 22:39:29 +0000

    add LICENSE, instructions in README.md, and some minor change in main.py)

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
 # Welcome home!
 
-This whole project needs to run on Rasbperry Pi. It uses the camera to take image at certain interval, upload them to AWS Rekognition to see any faces are detected. If any faces is detected, it then proceed to match the faces (i.e. the metadata of the faces) stored in DynamoDB. In case of a match, the program  then pulls the person's name in the DynamoDB, and send it to AWS Polly to be synthesized into a mp3 file. The mp3 file is then played on Rasbperry Pi to speak the persons' name.
+This project uses the camera on a Rasbperry Pi to taking pictures, and recognize persons' face in the picture. In case any faces matching the ones on record, it will play an audio to speak the person's name. 
 
-## Usage
 
-To be updated.
+## More info wanted?
+
+In case you want to know more about what happens behind the scenes, please read on. Otherwise, you can skip to the [Prerequisites](#prerequisites) Prerequisites section.
+
+The main program runs on an Rasbperry Pi with a camera connected. It uses the camera to take image continously, and upload them to AWS Rekognition to detect faces. Each face will be assigned with an unique UUID, which is used by Rekognition to identify the person. This unique id is also used as the partition key in DynamoDB, where the metadata of the person is stored. In case the detected faces match any faces stored in DynamoDB, the persons' name will be sent to AWS Polly to be synthesized into a mp3 file. The mp3 file is then played on Rasbperry Pi.
+
+
+##<a name="prerequisites"></a>Prerequisites
+
+1. An AWS account.
+2. Rasbperry Pi with a camera. (preferably with 1080P resolution)
+
+## Installation
+
+**Note.**
+
+>The following process (except for uploading the script to Rasbperry Pi and run it there) can be automated by using a CloudFormation template, which will be attached here later on.
+
+1. Create a S3 bucket to store face images.
+
+	Using AWS SDK or CLI to upload the image which should only contain the face of one person. 
+	
+	Please send the metadata (e.g. first name) of the person along with the image to S3. These attributes will be stored in the DynamoDB table, and will be used to synthesize the person' name.  
+	
+
+2. Create a Lambda function to index the faces in the image, and store the person's metadata in a DynamoDB table.
+
+
+	```python
+	lambda function code goes here.
+	```
+
+
+3. Use the Event Notification function on the S3 bucket to trigger the Lambda function created in step 2, to index the faces in the image.
+
+
+4. The face recognition program keeps running on the Raspberry Pi. From there, it keeps taking pictures, and calling AWS Rekognition service to detect faces in the image. If any faces detected in the image, they will be cropped out individually, and sent to match the faces stored in the DynamoDB table. 
+
+	In case any matching faces found in DynamoDB table, the first name of the individual person to which each face belongs to, will be concatenated and sent to AWS Polly for speech synthesizing.
+	
+	The default text synthesized is,
+	
+	"Hello [NAMEs], did you have a good day today?" 
+	
+	The synthesized audio will be stored locally on Rasbperry Pi, and played out via the speaker.
+	
--- main.py
+++ main.py
@@ -8,114 +8,115 @@ import uuid
 import io
 import subprocess
 
+class WelcomeHome(object):
 
-def detect_faces(byte_image):
+    def detect_faces(byte_image):
 
-    response = rekognition.detect_faces(
-        Image={
-            'Bytes': byte_image
-        }
-    )
-
-    faces = response['FaceDetails']
-
-    return faces
-
-
-def identify_faces(image, faces):
-
-    person = []
-
-    box = []
-
-    # Get image diameters
-    image_width = image.size[0]
-    image_height = image.size[1]
-
-    # Crop face from image
-    for face in faces:
-        box = face['BoundingBox']
-        x1 = int(box['Left'] * image_width) * 0.9
-        y1 = int(box['Top'] * image_height) * 0.9
-        x2 = int(box['Left'] * image_width + box['Width'] * image_width) * 1.10
-        y2 = int(box['Top'] * image_height + box['Height'] * image_height) * 1.10
-        image_crop = image.crop((x1, y1, x2, y2))
-        #
-        stream = io.BytesIO()
-        image_crop.save(stream, format="JPEG")
-        image_crop_binary = stream.getvalue()
-
-        # Submit individually cropped image to Amazon Rekognition
-        response = rekognition.search_faces_by_image(
-            CollectionId='family_collection',
-            Image={'Bytes': image_crop_binary}
+        response = rekognition.detect_faces(
+            Image={
+                'Bytes': byte_image
+            }
         )
 
-        if len(response['FaceMatches']) > 0:
+        faces = response['FaceDetails']
 
-            print('Coordinates ', box)
-            for match in response['FaceMatches']:
+        return faces
 
-                face = dynamodb.get_item(
-                    TableName='family_collection',
-                    Key={'RekognitionId': {'S': match['Face']['FaceId']}}
-                )
 
-                if 'Item' in face:
-                    person_name = face['Item']['FirstName']['S']
+    def identify_faces(image, faces):
 
-                    if person_name == 'Juan':
-                        person.append(person_name)
-                        return person
+        person = []
+
+        box = []
+
+        # Get image diameters
+        image_width = image.size[0]
+        image_height = image.size[1]
+
+        # Crop face from image
+        for face in faces:
+            box = face['BoundingBox']
+            x1 = int(box['Left'] * image_width) * 0.9
+            y1 = int(box['Top'] * image_height) * 0.9
+            x2 = int(box['Left'] * image_width + box['Width'] * image_width) * 1.10
+            y2 = int(box['Top'] * image_height + box['Height'] * image_height) * 1.10
+            image_crop = image.crop((x1, y1, x2, y2))
+            #
+            stream = io.BytesIO()
+            image_crop.save(stream, format="JPEG")
+            image_crop_binary = stream.getvalue()
+
+            # Submit individually cropped image to Amazon Rekognition
+            response = rekognition.search_faces_by_image(
+                CollectionId='family_collection',
+                Image={'Bytes': image_crop_binary}
+            )
+
+            if len(response['FaceMatches']) > 0:
+
+                print('Coordinates ', box)
+                for match in response['FaceMatches']:
+
+                    face = dynamodb.get_item(
+                        TableName='family_collection',
+                        Key={'RekognitionId': {'S': match['Face']['FaceId']}}
+                    )
+
+                    if 'Item' in face:
+                        person_name = face['Item']['FirstName']['S']
+
+                        if person_name == 'Juan':
+                            person.append(person_name)
+                            return person
+                        else:
+                            person.append(person_name)
+                            print(match['Face']['FaceId'], match['Face']['Confidence'], person)
                     else:
-                        person.append(person_name)
-                        print(match['Face']['FaceId'], match['Face']['Confidence'], person)
-                else:
-                    print("You've added the family photo in S3. However, you might forgotten to add" +
-                          person + "to the family list in the DynamoDB table.")
+                        print("You've added the family photo in S3. However, you might forgotten to add" +
+                              person + "to the family list in the DynamoDB table.")
+            else:
+                print("This person isn't in your family collection. You may consider to add a new collection.")
+
+        return person
+
+
+    def call_person(person):
+
+        names = ''
+
+        for i in person:
+            names = names + ' ' + i
+
+        if names.strip() == 'Juan':
+            input_text = "hola juan, has tenido un buen día hoy?"
         else:
-            print("This person isn't in your family collection. You may consider to add a new collection.")
+            input_text = "Welcome home " + names + ", did you have a good day today?"
 
-    return person
+        response = polly.synthesize_speech(OutputFormat='mp3', Text=input_text, VoiceId='Joanna')
+        audio_bytes = response['AudioStream'].read()
 
+        with open('./callmyname.mp3', 'wb') as fw:
+            fw.write(audio_bytes)
 
-def call_person(person):
-
-    names = ''
-
-    for i in person:
-        names = names + ' ' + i
-
-    if names.strip() == 'Juan':
-        input_text = "hola juan, has tenido un buen día hoy?"
-    else:
-        input_text = "Welcome home " + names + ", did you have a good day today?"
-
-    response = polly.synthesize_speech(OutputFormat='mp3', Text=input_text, VoiceId='Joanna')
-    audio_bytes = response['AudioStream'].read()
-
-    with open('./callmyname.mp3', 'wb') as fw:
-        fw.write(audio_bytes)
-
-    subprocess.call(['mpg321', './callmyname.mp3'])
+        subprocess.call(['mpg321', './callmyname.mp3'])
 
 
-def put_image(bucket, byte_image):
+    def put_image(bucket, byte_image):
 
-    s3.put_object(
-        Body=byte_image,
-        Bucket=bucket,
-        Key='rekognition/archive/' + str(uuid.uuid4()) + '_' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.jpeg'
-    )
+        s3.put_object(
+            Body=byte_image,
+            Bucket=bucket,
+            Key='rekognition/archive/' + str(uuid.uuid4()) + '_' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.jpeg'
+        )
 
 
-rekognition = boto3.client('rekognition', region_name='eu-west-1')
-s3 = boto3.client('s3')
-dynamodb = boto3.client('dynamodb', region_name='eu-west-1')
-polly = boto3.client('polly', region_name='eu-west-1')
+    rekognition = boto3.client('rekognition', region_name='eu-west-1')
+    s3 = boto3.client('s3')
+    dynamodb = boto3.client('dynamodb', region_name='eu-west-1')
+    polly = boto3.client('polly', region_name='eu-west-1')
 
-my_bucket = 'tlelet.com'
-local_image = '/tmp/image.jpeg'
+    my_bucket = 'tlelet.com'
+    local_image = '/tmp/image.jpeg'
 
 while True:
     with PiCamera() as camera:
